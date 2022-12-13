373e92506ff6d85704137b7891f0a85289a24eaa
Fabian <fabiananguiano@gmail.com>
2022-12-13 13:18:41 -0600

    Updated to include error handling

--- backup_r.py
+++ backup_r.py
@@ -1,6 +1,7 @@
 # Import the necessary modules
 import os
 import subprocess
+import telegram_send
 
 # Set the database to be backed up
 db_name = 'my_database_name'
@@ -12,10 +13,19 @@ backup_dir = '/path/to/backup/directory'
 backup_file = '{}-backup.zip'.format(db_name)
 
 # Create the backup command
-backup_cmd = 'odoo-bin -d {} -o -r admin -w admin --db_host=localhost --db_port=8069 --db_user=odoo --db_password=odoo --addons-path=../odoo/addons -r admin -w admin -b {} -f {}'.format(db_name, backup_dir, backup_file)
+backup_cmd = 'odoo-bin -d {} -o -r admin -w admin --db_host=localhost --db_port=5432 --db_user=odoo --db_password=odoo --addons-path=../odoo/addons -r admin -w admin -b {} -f {}'.format(db_name, backup_dir, backup_file)
 
-# Run the backup command
-subprocess.run(backup_cmd, shell=True)
+# Try to run the backup command
+try:
+    subprocess.run(backup_cmd, shell=True)
+    # Send a notification message
+    telegram_send.send(messages=['Odoo backup completed successfully.'])
+
+# Catch any errors that occur
+except Exception as e:
+    # Send a notification message with the error details
+    telegram_send.send(messages=['Odoo backup failed with error: {}'.format(e)])
+
 
 
 ## Cron job
