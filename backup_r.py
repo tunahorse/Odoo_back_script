676727cc3db3e323005255c1b6d04ccfcf5de8f7
Fabian <fabiananguiano@gmail.com>
2022-12-13 13:18:55 -0600

    Update backup_r.py

--- backup_r.py
+++ backup_r.py
@@ -13,7 +13,7 @@ backup_dir = '/path/to/backup/directory'
 backup_file = '{}-backup.zip'.format(db_name)
 
 # Create the backup command
-backup_cmd = 'odoo-bin -d {} -o -r admin -w admin --db_host=localhost --db_port=5432 --db_user=odoo --db_password=odoo --addons-path=../odoo/addons -r admin -w admin -b {} -f {}'.format(db_name, backup_dir, backup_file)
+backup_cmd = 'odoo-bin -d {} -o -r admin -w admin --db_host=localhost --db_port=8069 --db_user=odoo --db_password=odoo --addons-path=../odoo/addons -r admin -w admin -b {} -f {}'.format(db_name, backup_dir, backup_file)
 
 # Try to run the backup command
 try:
